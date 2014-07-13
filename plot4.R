d871f86ff4fe57815210e07023d816dcfaceb25f
joseabf <joseabf@gmail.com>
2014-07-13 17:18:54 +0200

    Last changes

--- plot4.R
+++ plot4.R
@@ -16,17 +16,20 @@ data$Date_Time<-ymd_hms(data$Date_Time)
 
 png(file="plot4.png", width=480, height=480)
 
+## build the four parts of screen
 par(mfrow=c(2,2))
 
+## plot the four graphics
+
 plot(data$Date_Time,data$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
+
+plot(data$Date_Time,data$Voltage,type="l",ylab="Voltage",xlab="datetime")
 
 plot(data$Date_Time,data$Sub_metering_1,xlab="",ylab="Energy sub metering", type="n")
 points(data$Date_Time,as.double(as.character(data$Sub_metering_1)),type="l",col="black")
 points(data$Date_Time,as.double(as.character(data$Sub_metering_2)),type="l",col="red")
 points(data$Date_Time,as.double(as.character(data$Sub_metering_3)),type="l",col="blue")
 legend("topright", col = c("black", "red", "blue"), lty= "solid", inset = .05, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") )
-
-plot(data$Date_Time,data$Voltage,type="l",ylab="Voltage",xlab="datetime")
 
 plot(data$Date_Time,data$Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime")
 
